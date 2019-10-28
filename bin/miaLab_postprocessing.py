65d5e74f1050d80f1316872d9602c5625d33dd5b
Basil <basil.peterhans@gmail.com>
2019-10-28 13:40:01 +0100

    added label masking loop

--- bin/miaLab_postprocessing.py
+++ bin/miaLab_postprocessing.py
@@ -1,6 +1,7 @@
 import os
 import SimpleITK as sitk
 import numpy as np
+import miaLab_sliceViewer as sv
 import matplotlib.pyplot as plt
 
 directory = "mia-result\\2019-10-21-12-03-21"
@@ -10,10 +11,31 @@ for filename in os.listdir(directory):
         print(os.path.join(directory, filename))
         img = sitk.ReadImage(os.path.join(directory, filename))
 
-        ccfilt = sitk.ConnectedComponentImageFilter()
-        ccImg = ccfilt.Execute(img)
-
+        ccFilt = sitk.ConnectedComponentImageFilter()
+        ccImg = ccFilt.Execute(img)
         arrCC = np.transpose(sitk.GetArrayFromImage(ccImg), [2, 1, 0])
+
+        sFilt = sitk.LabelStatisticsImageFilter()
+        sFilt.Execute(img, ccFilt)
+
+        # - 0 (background)
+        # - 1 (white matter)
+        # - 2 (grey matter)
+        # - 3 (Hippocampus)
+        # - 4 (Amygdala)
+        # - 5 (Thalamus)
+        labelNames = ["background", "white matter", "grey matter", "hippocampus", "amygdala", "thalamus"]
+
+        #TODO: imagestatisticsfilter (for sorting labels)
+        #TODO: Make loop for all labels
+        for idx in range(3, 6):
+            imgHy = (img == idx)
+            ccFiltHy = sitk.ConnectedComponentImageFilter()
+            ccFiltHy.Execute(imgHy)
+            print(labelNames[idx])
+            print(ccFiltHy.GetObjectCount())
+
+
 
         continue
     else:
