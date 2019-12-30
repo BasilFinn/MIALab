67e66014b5e7801282b2f993c59b4ee6f0e3ea63
S. Salzmann <simon.salzmann1993@gmail.com>
2019-12-30 09:07:08 +0100

    added eps images of heatmaps

--- matlab/showHeatmap.m
+++ matlab/showHeatmap.m
@@ -14,7 +14,7 @@ for lbl = 1:length(lbl_list)
         t_pp = t_pp(contains(t_pp.LABEL,lbl_list{lbl}),:);
         
         % Fill table content
-        diffHDRFDST(idx) = median(t_pp.HDRFDST)-median(t_org.HDRFDST);
+        diffHDRFDST(idx) = median(t_org.HDRFDST)-median(t_pp.HDRFDST);
 %         diffHDRFDST(idx) = median(t_pp.DICE)-median(t_org.DICE);
         csvSize{idx}  = csv.name(9:10);
         csvDepth{idx} = csv.name(12:13);
@@ -33,10 +33,10 @@ for lbl = 1:length(lbl_list)
     figure(lbl+100);
     imagesc(img);
     title(lbl_list{lbl});
-    xlabel('Tree Size');
+    xlabel('# Tree');
     ylabel('Tree Depth');
     cb = colorbar;
-    title(cb,'HDRFDST')
+    title(cb,'HDRFDST [mm]')
 %     title(cb,'DICE')
     
 end
